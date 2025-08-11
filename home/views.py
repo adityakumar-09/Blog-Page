d043eba4165e88963f8baf9d496b74b1e00a2b69
adityakumar-09 <ashishadi2005@gmail.com>
2025-08-11 08:01:20 +0530

    added search

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from home.models import Contact
 from django.contrib import messages
+from blog.models import Post
+
 # Create your views here.
 def home(request): 
     return render(request, 'home/home.html')
@@ -22,3 +24,17 @@ def contact(request):
 
 def about(request): 
     return render(request, 'home/about.html')
+#icontains used to get records that contains a specified value.
+def search(request):
+    query=request.GET['query']
+    if len(query)>78:
+        allPosts=Post.objects.none()
+    else:
+        allPostsTitle= Post.objects.filter(title__icontains=query)
+        allPostsAuthor= Post.objects.filter(author__icontains=query)
+        allPostsContent =Post.objects.filter(content__icontains=query)
+        allPosts=  allPostsTitle.union(allPostsContent, allPostsAuthor)
+    if allPosts.count()==0:
+        messages.warning(request, "No search results found. Please refine your query.")
+    params={'allPosts': allPosts, 'query': query}
+    return render(request, 'home/search.html', params)
